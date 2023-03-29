975cbd954f1988942e42468b91a4f8b3eaa55ca8
Artem Fadeev <126018223+fadeev19190@users.noreply.github.com>
2023-03-29 16:29:01 +0200

    Add files via upload

---
+++ speed.py
@@ -0,0 +1,20 @@
+def check_speed():
+    allowed_speed = 70
+    above_speed = 5
+    demerit = 0
+   # ride_license = True
+    while demerit < 12:
+        speed = int(input("Your speed: "))
+        if speed <= allowed_speed:
+            print("OK")
+            yield demerit
+        elif speed > allowed_speed:
+            demerit += int((speed - allowed_speed) / above_speed)
+            yield demerit
+        elif demerit >= 12:
+            print("License suspended")
+            yield False, demerit
+
+
+check = check_speed()
+print(f"Points: {check}")
